503310eb4f77282335456a5726ef60f31f49940c
ksteele98 <kyleblazer13@gmail.com>
2025-06-15 21:34:28 -0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,26 +1,23 @@
+// firebase-messaging-sw.js
+
 importScripts('https://www.gstatic.com/firebasejs/10.7.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.2/firebase-messaging-compat.js');
 
+// Initialize Firebase inside the Service Worker
 firebase.initializeApp({
   apiKey: "AIzaSyCmtc69JDgCWtxPNgPU73Y4n7-asl6M72w",
   authDomain: "my-website-5dfa1.firebaseapp.com",
   projectId: "my-website-5dfa1",
   storageBucket: "my-website-5dfa1.appspot.com",
   messagingSenderId: "36790147861",
-  appId: "1:36790147861:web:6391e58fe3193c4fabe71c",
-  measurementId: "G-SSJEV8WQFT"
+  appId: "1:36790147861:web:6391e58fe3193c4fabe71c"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/mywebsite/icon-192x192.png' // Optional
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+// Handle background messages
+messaging.onBackgroundMessage(({ notification, data }) => {
+  const title = notification?.title ?? data?.title ?? 'Notification';
+  const body  = notification?.body  ?? data?.body  ?? '';
+  self.registration.showNotification(title, { body });
 });
