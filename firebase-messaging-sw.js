80eb5b71f3d2da5a4c7bc5507019748e98d76522
ksteele98 <kyleblazer13@gmail.com>
2025-06-15 21:01:17 -0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,34 +1,26 @@
-importScripts(
-  'https://www.gstatic.com/firebasejs/10.7.2/firebase-app-compat.js',
-  'https://www.gstatic.com/firebasejs/10.7.2/firebase-messaging-compat.js'
-);
+importScripts('https://www.gstatic.com/firebasejs/10.7.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.2/firebase-messaging-compat.js');
 
-// --- Firebase project config (same values you use in index.html) ---
 firebase.initializeApp({
-  apiKey:            "AIzaSyCmtc69JDgCWtxPNgPU73Y4n7-asl6M72w",
-  authDomain:        "my-website-5dfa1.firebaseapp.com",
-  projectId:         "my-website-5dfa1",
-  storageBucket:     "my-website-5dfa1.appspot.com",
+  apiKey: "AIzaSyCmtc69JDgCWtxPNgPU73Y4n7-asl6M72w",
+  authDomain: "my-website-5dfa1.firebaseapp.com",
+  projectId: "my-website-5dfa1",
+  storageBucket: "my-website-5dfa1.appspot.com",
   messagingSenderId: "36790147861",
-  appId:             "1:36790147861:web:6391e58fe3193c4fabe71c"
+  appId: "1:36790147861:web:6391e58fe3193c4fabe71c",
+  measurementId: "G-SSJEV8WQFT"
 });
 
-// Get messaging instance that works inside a SW
 const messaging = firebase.messaging();
 
-// ------------------------------------------------------------------
-// Background-message handler.
-// This fires when the page is closed or in the background.
-// ------------------------------------------------------------------
-messaging.onBackgroundMessage(({ notification }) => {
-  // Silent data message?  Nothing to display.
-  if (!notification) return;
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/mywebsite/icon-192x192.png' // Optional
+  };
 
-  self.registration.showNotification(
-    notification.title ?? 'Notification',
-    {
-      body: notification.body ?? '',
-      icon: '/icon-192.png'   // Path to one of your icons (optional)
-    }
-  );
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
